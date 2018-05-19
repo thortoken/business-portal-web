99d43a1e2738b52db604c439099d85d10ea70bb8
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-05-19 19:13:26 +0200

    task: Remove unnecessary index files

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,3 +1,40 @@
-import Dashboard from './Dashboard';
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default Dashboard;
+import { syncPayments } from '~redux/actions/dashboard';
+
+import './Dashboard.css';
+
+class Dashboard extends React.Component {
+  state = {
+    message: 'World',
+  };
+
+  componentDidMount() {
+    this.props.syncPayments();
+  }
+
+  render() {
+    return (
+      <div className="dashboard">
+        <div className="header">Dashboard</div>
+        <ul>{this.props.payments.map(payment => <li key={payment.id}>{payment.amount}</li>)}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  payments: state.dashboard.payments,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      syncPayments,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- src/routes/AuthorizedRoutes/index.js
+++ src/routes/AuthorizedRoutes/index.js
@@ -1,3 +1,29 @@
-import AuthorizedRoutes from './AuthorizedRoutes';
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { Layout } from 'antd';
 
-export default AuthorizedRoutes;
+import LandingPage from '~pages/LandingPage';
+import Dashboard from '~pages/Dashboard';
+import Payments from '~pages/Payments';
+
+import Sidebar from './components/Sidebar';
+
+import './AuthorizedRoutes.css';
+
+export default class AuthorizedRoutes extends React.Component {
+  render() {
+    return (
+      <Layout className="AuthorizedRoutes">
+        <Sidebar />
+        <Layout.Content className="AuthorizedRoutes-content">
+          <Switch>
+            <Route exact path="/landing" component={LandingPage} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/payments" component={Payments} />
+            <Redirect from="*" to="/dashboard" />
+          </Switch>
+        </Layout.Content>
+      </Layout>
+    );
+  }
+}
