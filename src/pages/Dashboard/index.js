7f654eb91e0ec495d88d04c4cdfc3a3d04546616
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-05-22 16:02:51 +0200

    feat: Fake placeholders (screenshots) on dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Row, Col, Icon, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import { Row, Col, Icon, Button, Divider } from 'antd';
 
 import { syncPayments } from '~redux/actions/dashboard';
 
 import Card from '~components/Card';
 import Header from '~components/Header';
+import Dropdown from '~components/Dropdown';
 
 class Dashboard extends React.Component {
   componentDidMount() {
@@ -48,10 +50,43 @@ class Dashboard extends React.Component {
             <Card description="Account Value" title="$20,013.65" color="green" rounded />
           </Col>
         </Row>
+        <Divider />
+        <Header title="Recent Payments" size="small">
+          <Header.Left>
+            <Dropdown options={['DAY', 'MONTH', 'YEAR']} onClick={this.handlePaymentsPeriodChange}>
+              <Button type="primary" ghost>
+                MONTH <Icon type="down" />
+              </Button>
+            </Dropdown>{' '}
+            <Button type="primary">Create Report</Button>
+          </Header.Left>
+          <Header.Right>
+            <Link to="#">View All Payments &rarr;</Link>
+          </Header.Right>
+        </Header>
+        <div>
+          <img src="/images/thor_charts.png" width="100%" />
+        </div>
+        <Divider />
+        <Header title="Satisfaction Rates" size="small">
+          <Header.Left>
+            <Button type="primary">Conduct Survey</Button>
+          </Header.Left>
+          <Header.Right>
+            <Link to="#">View All Surveys &rarr;</Link>
+          </Header.Right>
+        </Header>
+        <div>
+          <img src="/images/thor_satisfaction_rates.png" width="100%" />
+        </div>
         <ul>{this.props.payments.map(payment => <li key={payment.id}>{payment.amount}</li>)}</ul>
       </div>
     );
   }
+
+  handlePaymentsPeriodChange = ({ key }) => {
+    console.log('handlePaymentsPeriodChange', key);
+  };
 }
 
 const mapStateToProps = state => ({
