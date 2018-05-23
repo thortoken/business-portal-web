5af786c6ca3b59789a2cf6f38a5b1ea2bd8993d5
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-05-23 18:02:51 +0200

    style: Box - transparent variant

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -4,8 +4,12 @@ import './Box.css';
 
 export default class Box extends React.Component {
   render() {
-    const { className } = this.props;
+    const { className, transparent } = this.props;
 
-    return <div className={`Box ${className}`}>{this.props.children}</div>;
+    return (
+      <div className={`Box ${className} ${transparent ? 'Box--transparent' : ''}`}>
+        {this.props.children}
+      </div>
+    );
   }
 }
