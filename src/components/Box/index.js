d2b4e52e3f3052417c48fe383dffe9d1867db0d6
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-05-24 15:10:20 +0200

    fix: Add missing default prop for Box className

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -4,7 +4,7 @@ import './Box.css';
 
 export default class Box extends React.Component {
   render() {
-    const { className, transparent } = this.props;
+    const { className = '', transparent } = this.props;
 
     return (
       <div className={`Box ${className} ${transparent ? 'Box--transparent' : ''}`}>
