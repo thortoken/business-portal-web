06fb1f58be6cfdb43775408e718e7c2504baa75f
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-06-01 13:20:51 +0200

    test: Set default locale to en-US

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,3 +2,9 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+
+Object.defineProperty(navigator, 'language', {
+  get() {
+    return 'en-US';
+  },
+});
