5997f91fca77fedc49c32a60b61194b0321396eb
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-06-04 12:58:40 +0200

    task: Add redux-form package

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import { routerMiddleware, routerReducer } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
+import { reducer as formReducer } from 'redux-form';
 
 import loginReducer from './reducers/login';
 import jumioReducer from './reducers/jumio';
@@ -16,6 +17,7 @@ export const history = createHistory();
 const sagaMiddleware = createSagaMiddleware();
 
 const allReducers = combineReducers({
+  form: formReducer,
   login: loginReducer,
   jumio: jumioReducer,
   status: statusReducer,
