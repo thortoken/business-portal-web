728e23ac322c21c2e85ba116d9cab12d5dc00234
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-06-08 13:26:31 +0200

    feat: Redux saga pagination query util

--- src/redux/sagas/utils.js
+++ src/redux/sagas/utils.js
@@ -1,3 +1,17 @@
 export const documentTransformer = doc => ({ id: doc.id, ...doc.data() });
 export const collectionTransformer = (snapshot, docTransformer = documentTransformer) =>
   snapshot.docs.map(docTransformer);
+
+export const paginateQuery = (firebaseQuery, { page = 1, limit, orderBy }) => {
+  if (limit < 1) {
+    throw new Error('Limit should be a positive integer.');
+  }
+  if (!Array.isArray(orderBy)) {
+    orderBy = [orderBy];
+  }
+
+  return firebaseQuery
+    .limit(limit)
+    .orderBy(...orderBy)
+    .startAfter(limit * (page - 1));
+};
--- src/redux/sagas/utils.spec.js
+++ src/redux/sagas/utils.spec.js
@@ -19,6 +19,12 @@ const createFakeCollection = length => {
   return { docs: collection };
 };
 
+const firestoreQueryMock = () => ({
+  limit: jest.fn().mockReturnThis(),
+  orderBy: jest.fn().mockReturnThis(),
+  startAfter: jest.fn().mockReturnThis(),
+});
+
 describe('redux: saga utils', () => {
   describe('documentTransformer', () => {
     it('should unpack document data', () => {
@@ -59,6 +65,58 @@ describe('redux: saga utils', () => {
           third: '3',
         });
       });
+    });
+  });
+
+  describe('paginateQuery', () => {
+    let fakeQuery;
+
+    beforeEach(() => {
+      fakeQuery = firestoreQueryMock();
+    });
+
+    it('should inject pagination conditions to the query', () => {
+      const options = {
+        limit: 10,
+        page: 3,
+        orderBy: 'name',
+      };
+
+      utils.paginateQuery(fakeQuery, options);
+
+      expect(fakeQuery.limit).toHaveBeenCalledWith(10);
+      expect(fakeQuery.orderBy).toHaveBeenCalledWith('name');
+      expect(fakeQuery.startAfter).toHaveBeenCalledWith(10 * (3 - 1));
+    });
+
+    it('should allow passing the ordering direction', () => {
+      const options = {
+        limit: 10,
+        page: 3,
+        orderBy: ['name', 'desc'],
+      };
+
+      utils.paginateQuery(fakeQuery, options);
+
+      expect(fakeQuery.limit).toHaveBeenCalledWith(10);
+      expect(fakeQuery.orderBy).toHaveBeenCalledWith('name', 'desc');
+      expect(fakeQuery.startAfter).toHaveBeenCalledWith(10 * (3 - 1));
+    });
+
+    it('should throw an exception on invalid options', () => {
+      const options = {
+        limit: 0,
+        page: 3,
+        orderBy: 'name',
+      };
+
+      expect(() => utils.paginateQuery(fakeQuery, options)).toThrowError(
+        'Limit should be a positive integer.'
+      );
+
+      expect(fakeQuery.limit).not.toHaveBeenCalled();
+      expect(fakeQuery.orderBy).not.toHaveBeenCalled();
+      expect(fakeQuery.startAfter).not.toHaveBeenCalled();
     });
   });
 });
