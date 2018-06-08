ce80fb2bfddb4ed45b8a743bf89c738c03cefd2e
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-06-08 18:09:06 +0200

    task: Antd package mocks for unit tests

---
+++ __mocks__/antd.js
@@ -0,0 +1,29 @@
+const namedComponent = name => {
+  const Component = ({ children }) => children;
+  Component.displayName = name;
+  return Component;
+};
+
+const Icon = namedComponent('Icon');
+
+const Layout = namedComponent('Layout');
+Layout.Content = namedComponent('Content');
+Layout.Sider = namedComponent('Sider');
+
+const Menu = namedComponent('Menu');
+Menu.Item = namedComponent('MenuItem');
+
+const Steps = namedComponent('Steps');
+Steps.Step = namedComponent('Step');
+
+const Table = namedComponent('Table');
+Table.Column = namedComponent('Column');
+Table.Row = namedComponent('Row');
+
+module.exports = {
+  Layout,
+  Menu,
+  Icon,
+  Steps,
+  Table,
+};
