8fb344232c0d95f27e1f09e943ebeed59644477d
jakubdrozdek <30927218+jakubdrozdek@users.noreply.github.com>
2018-07-20 14:51:46 +0200

    style: Change site navigation orientation to horizontal #THOR-43 (#3)
    * style: Fixed-width but responsive site content
    * style: Change site navigation orientation to horizontal

--- src/routes/AuthorizedRoutes/components/Sidebar/index.js
+++ src/routes/AuthorizedRoutes/components/Sidebar/index.js
@@ -24,76 +24,51 @@ export class Sidebar extends React.Component {
     pathname: PropTypes.string,
   };
 
-  state = {
-    collapsed: true,
-  };
-
   render() {
-    const { collapsed } = this.state;
     const { pathname } = this.props;
 
     const selectedKeys = [getSelectedKeyFromPath(pathname)];
 
     return (
-      <div>
-        <Layout.Sider
-          className="Sidebar"
-          onMouseOver={this.handleMouseOver}
-          onMouseOut={this.handleMouseOut}
-          collapsed={collapsed}
-          width="300px">
-          <Menu theme="dark" mode="inline" selectedKeys={selectedKeys}>
-            <Menu.Item className="Sidebar-header">
-              <div className="Sidebar-header-brand">
-                <img className="anticon" src="images/THOR-icon.png" />
-                <span>Luber</span>
-              </div>
-            </Menu.Item>
-            <Menu.Item key="/welcome">
-              <Link to="/welcome">
-                <Icon type="notification" />
-                <span>Welcome!</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/dashboard">
-              <Link to="/dashboard">
-                <Icon type="home" />
-                <span>Account Summary</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/contractors">
-              <Link to="#">
-                <Icon type="solution" />
-                <span>Contractors</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/payments">
-              <Link to="/payments">
-                <Icon type="table" />
-                <span>Payments</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/satisfaction-surveys">
-              <Link to="/satisfaction-surveys">
-                <Icon type="smile" />
-                <span>Satisfaction Surveys</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item key="/accounts">
-              <Link to="#">
-                <Icon type="setting" />
-                <span>Connected Accounts</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item className="Sidebar-item-logout">
-              <a href="#" onClick={this.handleLogout}>
-                <Icon type="logout" />
-                <span>Logout</span>
-              </a>
-            </Menu.Item>
-          </Menu>
-        </Layout.Sider>
-      </div>
+      <Layout.Header className="Sidebar">
+        <Menu theme="light" mode="horizontal" selectedKeys={selectedKeys}>
+          <Menu.Item className="Sidebar-header">
+            <div className="Sidebar-header-brand">
+              <img className="anticon" src="images/THOR-icon.png" />
+            </div>
+          </Menu.Item>
+          <Menu.Item key="/welcome">
+            <Link to="/welcome">
+              <Icon type="notification" />
+              <span>Welcome!</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/dashboard">
+            <Link to="/dashboard">
+              <Icon type="home" />
+              <span>Account Summary</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/payments">
+            <Link to="/payments">
+              <Icon type="table" />
+              <span>Payments</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/satisfaction-surveys">
+            <Link to="/satisfaction-surveys">
+              <Icon type="smile" />
+              <span>Satisfaction Surveys</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item className="Sidebar-item-logout">
+            <a href="#" onClick={this.handleLogout}>
+              <Icon type="logout" />
+              <span>Logout</span>
+            </a>
+          </Menu.Item>
+        </Menu>
+      </Layout.Header>
     );
   }
 
