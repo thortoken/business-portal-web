3acab5b66035a9d3e08c17b52686a1d233b46bf4
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-07-23 11:31:13 +0200

    feat: Add Progress component with label above the bar

---
+++ src/components/Progress/Progress.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Progress from './index';
+
+describe('component: <Progress/>', () => {
+  describe('Progress', () => {
+    it('should render without crashing', () => {
+      const wrapper = shallow(<Progress />);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should pass all the other props to the Ant Progress component', () => {
+      const wrapper = shallow(<Progress percent={20} status="exception" />);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render the percent text in the middle', () => {
+      const wrapper = shallow(<Progress percent={20} status="exception" />);
+      expect(wrapper.find('.Progress-text').prop('style')).toEqual({ left: '10%' });
+    });
+  });
+});
