3c5e5e320ca187a35e6bbfe72f56051b07545c34
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-08-27 18:29:05 +0200

    feat: Add Notification service Related to #THOR-107

---
+++ src/services/notification.js
@@ -0,0 +1,29 @@
+import { notification } from 'antd';
+
+// Defaults
+notification.config({
+  placement: 'topRight',
+  duration: 3,
+});
+
+const generateNotificationKey = () => `notif-${+new Date()}`;
+
+const openAndReturnKey = args => {
+  const key = generateNotificationKey();
+  notification.open({ ...args, key });
+  return key;
+};
+
+const NotificationService = {
+  open: openAndReturnKey,
+  close: notification.close,
+  destroy: notification.destroy,
+};
+
+['success', 'warning', 'info', 'error'].forEach(type => {
+  NotificationService[type] = args => openAndReturnKey({ ...args, type });
+});
+
+NotificationService.warn = NotificationService.warning;
+
+export default NotificationService;
