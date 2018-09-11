cbb0e0c6f85cbb2c37d460c24ed23625fd55f927
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-09-11 16:10:35 +0200

    feat: Allow formatting and parsing form field values

--- src/components/FormField/FormField.js
+++ src/components/FormField/FormField.js
@@ -10,8 +10,10 @@ export class FormField extends Component {
   static propTypes = {
     className: PropTypes.string,
     component: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
+    formatter: PropTypes.func,
     label: PropTypes.string,
     name: PropTypes.string.isRequired,
+    parser: PropTypes.func,
   };
 
   static defaultProps = {
@@ -19,13 +21,36 @@ export class FormField extends Component {
   };
 
   renderInput = ({ field, form }) => {
-    const { component: Component, className, name, label, ...inputProps } = this.props;
+    const {
+      component: Component,
+      className,
+      name,
+      label,
+      formatter,
+      parser,
+      ...inputProps
+    } = this.props;
+
+    let { value } = field;
+    // Format the displayed value.
+    if (typeof formatter === 'function') {
+      value = formatter(value);
+    }
+
+    let { onChange } = field;
+    // Parse back the formatted value just before sending it to Formik.
+    if (typeof parser === 'function') {
+      onChange = evt => {
+        evt.target.value = parser(evt.target.value);
+        return field.onChange(evt);
+      };
+    }
 
     const errors = form.touched[name] && form.errors[name];
 
     return (
       <InputGroup className={className} errors={errors} label={label}>
-        <Component name={name} {...field} {...inputProps} />
+        <Component name={name} {...field} value={value} onChange={onChange} {...inputProps} />
       </InputGroup>
     );
   };
