37bfb5cb77afee86ba7279efa04959c312d91410
MNAcaisoft <michal.nicer@acaisoft.com>
2018-09-18 14:00:52 +0200

    [THOR-177] - add refresh button (#57)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Header.css';
+import RefreshButton from '../RefreshButton';
 
 export class Header extends React.Component {
   static propTypes = {
@@ -9,6 +10,9 @@ export class Header extends React.Component {
     description: PropTypes.string,
     size: PropTypes.oneOf(['small', 'medium', 'large']),
     title: PropTypes.string,
+    handleRefresh: PropTypes.func,
+    refresh: PropTypes.bool.isRequired,
+    refreshLoading: PropTypes.bool,
   };
 
   static defaultProps = {
@@ -22,12 +26,27 @@ export class Header extends React.Component {
   };
 
   render() {
-    const { description, size, title, children } = this.props;
+    const {
+      description,
+      size,
+      title,
+      children,
+      refresh,
+      handleRefresh,
+      refreshLoading,
+    } = this.props;
     const HeaderComponent = Header.headerSizes[size];
 
     return (
       <header className="Header">
-        <HeaderComponent className="Header-title">{title}</HeaderComponent>
+        <HeaderComponent className="Header-title">
+          {title}
+          {refresh && (
+            <div className="Header-title__refresh">
+              <RefreshButton handleRefresh={handleRefresh} isLoading={refreshLoading} />
+            </div>
+          )}
+        </HeaderComponent>
         {children}
         {description && <p className="Header-description">{description}</p>}
       </header>
