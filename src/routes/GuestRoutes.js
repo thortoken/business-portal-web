6149bcbe6048e43678dd13305da72fa8915ab5cb
wokeGit <wkalemba.net@gmail.com>
2018-09-20 13:15:32 +0200

    Added register flow

--- src/routes/GuestRoutes.js
+++ src/routes/GuestRoutes.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import LandingPage from '~pages/LandingPage';
 import Login from '~pages/Login';
+import UserInfo from '~pages/UserInfo';
+import Status from '~pages/Status';
+import RegisterPage from '../pages/Register';
 
 export class GuestRoutes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/sign-in" component={Login} />
+        <Route exact path="/userinfo" component={UserInfo} />
+        <Route exact path="/status" component={Status} />
+        <Route path="/register" component={RegisterPage} />
         <Redirect from="*" to="/sign-in" />
       </Switch>
     );
