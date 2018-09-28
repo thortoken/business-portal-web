c7f15a8baad3f53f1856362172ba924194ce7cb1
Wojciech Kalemba <wojciech.kalemba@acaisoft.com>
2018-09-28 14:53:03 +0200

    fix: back btn should move back to previous screen (#68)

--- src/components/BackBtn/index.js
+++ src/components/BackBtn/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Icon } from 'antd';
-import { Link } from 'react-router-dom';
 
 import './BackBtn.css';
 
 class BackBtn extends React.Component {
   render() {
-    const { to, label } = this.props;
-
     return (
       <div className="BackBtn">
-        <Link to={to}>
-          <Icon type="left" /> {label}
-        </Link>
+        <a onClick={this.handleBackBtn}>
+          <Icon type="left" /> Back
+        </a>
       </div>
     );
   }
-}
 
-BackBtn.propTypes = {
-  to: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-};
+  handleBackBtn = () => {
+    const { history } = this.props;
+    history.goBack();
+  };
+}
 
 export default BackBtn;
